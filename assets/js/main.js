70b80ff16c2fb6ac03d404cced4cea9e66771123
unknown <aaronaguilar12721@gmail.com>
2024-02-21 12:34:54 -0600

    Removed last lorem, not sure if well keep this text, reworked the navbar to now have animated scrolling and a menu for mobile users, added some animations to give more impact to the buttons and changed some of the backgrounds to give the text more readability and doge, fixed some css stuf

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,3 +161,43 @@ function changeStyle() {
 }
 
 setInterval(changeStyle, 3000);
+
+//mobile-menu-btn
+let navbarToggler = document.querySelector(".mobile-menu-btn");
+navbarToggler.addEventListener('click', function () {
+    navbarToggler.classList.toggle("active");
+});
+
+// for menu scroll 
+var pageLink = document.querySelectorAll('.page-scroll');
+
+pageLink.forEach(elem => {
+    elem.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(elem.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            offsetTop: 1 - 60,
+        });
+    });
+});
+
+// section menu active
+function onScroll(event) {
+    var sections = document.querySelectorAll('.page-scroll');
+    var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (var i = 0; i < sections.length; i++) {
+        var currLink = sections[i];
+        var val = currLink.getAttribute('href');
+        var refElement = document.querySelector(val);
+        var scrollTopMinus = scrollPos + 73;
+        if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
+            document.querySelector('.page-scroll').classList.remove('active');
+            currLink.classList.add('active');
+        } else {
+            currLink.classList.remove('active');
+        }
+    }
+};
+
+window.document.addEventListener('scroll', onScroll);
